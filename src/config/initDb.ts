c0117b3889c6d7cdf609313873d84b76ae90b472
au-fahim <dev.aufahim@gmail.com>
2026-05-22 16:46:32 +0600

    Setup DB Connection & Created Tables (Users & Issues)

---
+++ src/config/initDb.ts
@@ -0,0 +1,40 @@
+import pool from "./db";
+
+const createTables = async () => {
+  const usersTableQuery = `
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) UNIQUE NOT NULL,
+      password TEXT NOT NULL,
+      role VARCHAR(50) DEFAULT 'contributor' CHECK (role IN ('contributor', 'maintainer')),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `;
+
+  const issuesTableQuery = `
+    CREATE TABLE IF NOT EXISTS issues (
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(150) NOT NULL,
+      description TEXT NOT NULL,
+      type VARCHAR(50) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+      status VARCHAR(50) DEFAULT 'open' CHECK (status IN ('open', 'in_progress', 'resolved')),
+      reporter_id INTEGER NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `;
+
+  try {
+    await pool.query(usersTableQuery);
+    await pool.query(issuesTableQuery);
+    console.log("Tables created successfully!");
+    process.exit(0);
+  } catch (err) {
+    console.error("Error creating tables:", err);
+    process.exit(1);
+  }
+};
+
+createTables();
---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pool from "./config/db";
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "DevPulse is running!" });
+});
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`);
+  try {
+    const res = await pool.query("SELECT NOW()");
+    console.log("Database connection verified at:", res.rows[0].now);
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+});
